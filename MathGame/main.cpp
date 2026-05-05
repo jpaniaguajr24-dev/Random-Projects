36f03922ae308d33620dc6c9e9f082c4d532422a
jpaniaguajr24-dev <Paniaguajuanjr24@gmail.com>
2026-05-05 11:16:40 -0400

    Remove CLion help tip comment
    Removed a tip comment about CLion from the main.cpp file.

--- MathGame/main.cpp
+++ MathGame/main.cpp
@@ -387,4 +387,3 @@ int main(int argc, char *argv[])
     // Keep the app running until the player closes it
     return app.exec();
 }
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
